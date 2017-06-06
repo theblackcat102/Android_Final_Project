22435f240f2aca9b57eb2077a0a06c0f63c6161b
DBlackKat <zhirui09400@gmail.com>
2017-06-06 18:40:57 +0800

    fix padding and icon bug

--- app/src/main/java/com/toolers/toolers/MainActivity.java
+++ app/src/main/java/com/toolers/toolers/MainActivity.java
@@ -199,7 +199,6 @@ public class MainActivity extends AppCompatActivity
                             restaurantAdapter.setNewData(restaurantList);
                         }
                     });
-
                 }
             }
         });
