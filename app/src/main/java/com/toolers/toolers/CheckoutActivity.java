5d59459c5caabbab6ade04f1baee360dff4045c1
DBlackKat <zhirui09400@gmail.com>
2017-06-09 15:32:11 +0800

    java class starting...

--- app/src/main/java/com/toolers/toolers/CheckoutActivity.java
+++ app/src/main/java/com/toolers/toolers/CheckoutActivity.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
+import com.stripe.android.model.Card;
+import com.stripe.android.view.CardInputWidget;
 import com.toolers.toolers.adapter.CheckOutMainAdapter;
 import com.toolers.toolers.model.ShoppingCartModel;
 
@@ -18,13 +20,19 @@ public class CheckoutActivity extends AppCompatActivity {
     private CheckOutMainAdapter mainAdapter;
     private ListView additionalList;
     private ShoppingCartModel shoppingCart;
+    private Card card;
+    private CardInputWidget mCardInputWidget;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        mCardInputWidget = (CardInputWidget) findViewById(R.id.card_input_widget);
+        card =mCardInputWidget.getCard();
+        if(card == null){
+            // TODO : do error handling
+        }
         shoppingCart = getIntent().getExtras().getParcelable(MenuActivity.EXTRA_SHOPPING_CART);
         Log.d(TAG, "onCreate");
         mainList = (ListView) findViewById(R.id.main_list);
